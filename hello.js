8c97997da49c723c3527611b514096d4b76e008c
GEROMAX <tobita0112@gmail.com>
2018-05-15 14:56:22 +0900

    too beginners.

--- hello.js
+++ hello.js
@@ -1,1 +1,12 @@
-console.log("hello GEROMAX's learning world!", "what happen using multi parameter?");
+console.log(
+  "hello GEROMAX's learning world!",
+  "what happen using multi parameter?"
+);
+
+let firstName = "GERO";
+let lastName = "MAX";
+console.log(firstName, lastName);
+
+let city = "Kawasaki";
+let state = "Kanagawa";
+console.log(state + "\t" + city)
