fc80d9fdc366245f09c70fb9ca5c6cd05f682906
GEROMAX <tobita0112@gmail.com>
2018-05-28 13:50:57 +0900

    complete 7.1

--- palindrome.js
+++ palindrome.js
@@ -1,8 +1,18 @@
-const palindrome = str => {
-  let casingStr = str.toLowerCase();
-  return casingStr === reverseString(casingStr);
-};
-
 const reverseString = str => {
   return Array.from(str).reverse().join("");
 };
+
+// Defines a Phrase object.
+function Phrase(content) {
+  //property
+  this.content = content;
+
+  //function
+  this.palindrome = () => {
+    let processedContent = this.content.toLowerCase();
+    return processedContent === reverseString(processedContent);
+  };
+  this.louder = () =>{
+    return this.content.toUpperCase();
+  };
+}
