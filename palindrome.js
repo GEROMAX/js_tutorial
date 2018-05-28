5f8d0deb96f126868f162601a41670ea8975b6fc
GEROMAX <tobita0112@gmail.com>
2018-05-28 14:37:28 +0900

    complete 7.2

--- palindrome.js
+++ palindrome.js
@@ -1,5 +1,7 @@
 const reverseString = str => {
-  return Array.from(str).reverse().join("");
+  return Array.from(str)
+    .reverse()
+    .join("");
 };
 
 // Defines a Phrase object.
@@ -8,11 +10,27 @@ function Phrase(content) {
   this.content = content;
 
   //function
-  this.palindrome = () => {
-    let processedContent = this.content.toLowerCase();
-    return processedContent === reverseString(processedContent);
+  this.lowerStr = function lowerStr(str) {
+    return str.toLowerCase();
   };
-  this.louder = () =>{
+  this.lowerText = function lowerText() {
+    return this.lowerStr(this.content);
+  };
+  this.palindrome = function palindrome() {
+    return this.lowerText() === reverseString(this.lowerText());
+  };
+  this.louder = function louder() {
     return this.content.toUpperCase();
   };
 }
+
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // method override
+  this.lowerText = function lowerText() {
+    return this.lowerStr(this.translation);
+  };
+}
+TranslatedPhrase.prototype = new Phrase();
